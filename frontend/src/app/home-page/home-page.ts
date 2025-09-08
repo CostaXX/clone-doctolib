059075979830fb64ee94a4ff039ef220989a77a0
CostaXX <dulcaconstantin24@gmail.com>
2025-09-08 02:05:12 +0200

    feat: add header, footer, hero components and home page integration
    - Implemented header component with navigation and logo.
    - Created footer component with a simple message.
    - Developed hero component with a placeholder message.
    - Integrated hero component into the home page.
    - Added basic styles for header, footer, hero, and home page.

---
+++ frontend/src/app/home-page/home-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero/hero';
+
+@Component({
+  selector: 'app-home-page',
+  imports: [Hero],
+  templateUrl: './home-page.html',
+  styleUrl: './home-page.scss'
+})
+export class HomePage {
+  title = 'clone-doctolib';
+}
